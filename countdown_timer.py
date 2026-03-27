8455ad80814c1b49dd6ab6a7b8201976de04b2a6
Arin <arintembe@gmail.com>
2026-03-27 15:54:00 +0530

    countdown_timer

---
+++ countdown_timer.py
@@ -0,0 +1,15 @@
+import time
+
+def countdown(t):
+    while t: 
+        mins, secs = divmod(t, 60)
+        hours, mins = divmod(mins, 60)
+        timer = '{:02d}:{:02d}:{:02d}'.format(hours, mins, secs)
+        print(timer, end = '\r')
+        time.sleep(1)
+        t -= 1
+
+    print("Timer completed get back to work!")
+
+t = int(input("Enter time in seconds: "))
+countdown(int(t))
